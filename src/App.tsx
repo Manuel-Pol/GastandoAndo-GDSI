6059e7632d92b7b98cc4be8cd22481a74c380140
Martin Reimundo <mreimundo@itaptec.com.ar>
2024-05-01 20:38:26 -0300

    add login y react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Login from './components/login/Login'
 
 function App() {
 
   return (
-    <>
-        <p className="font bold text-5xl">Hola Mundo! XD</p>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/components/login/Login.tsx
@@ -0,0 +1,13 @@
+
+
+const Login = () => {
+
+    return (
+        <div>
+            <p className="font bold text-5xl">login</p>
+        </div>
+    )
+}
+
+
+export default Login
