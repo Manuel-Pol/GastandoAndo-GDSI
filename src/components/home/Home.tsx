c8e4dda2c46ba9885cda0a19da59f7861375d44d
alendavies <alendavies10@gmail.com>
2024-05-23 16:05:20 -0300

    home page icons

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,3 +1,4 @@
+import { Calculator, User, Users } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -13,21 +14,39 @@ const HomeLanding: React.FC = () => {
     }
 
     return (
-        <div className='flex flex-col items-center justify-center h-full py-12 gap-8'>
+        <div className='flex flex-col items-center justify-center h-full py-12 gap-14'>
             <h1 className='text-7xl font-serif font-semibold text-[#0e3d26]'>Gastando Ando</h1>
-            <div className='flex flex-col space-y-8'>
-                <button
-                    onClick={goToPersonalExpenses}
-                    className='bg-[#1c7549] hover:bg-[#175e3a] text-white font-bold py-2 px-4 rounded'
-                >
-                    Ir a Gastos Personales
-                </button>
-                <button
-                    onClick={goToGroupExpenses}
-                    className='bg-[#1c7549] hover:bg-[#175e3a] text-white font-bold py-2 px-4 rounded mt-4'
-                >
-                    Ir a Gastos Grupales
-                </button>
+            <div className='flex flex-row gap-8 items-center'>
+                <div className='flex flex-col gap-3 items-center'>
+                    <div>
+                        <img src='/assets/individual1.jpeg' className='w-52 h-52 rounded' />
+                    </div>
+                    <button
+                        onClick={goToPersonalExpenses}
+                        className='bg-[#1c7549] hover:bg-[#175e3a] text-white font-bold py-2 px-4 rounded flex flex-row items-center gap-3'
+                    >
+                        <User /> <p>Gastos Personales</p>
+                    </button>
+                </div>
+                <div className='flex flex-col gap-3 items-center'>
+                    <div>
+                        <img src='/assets/grupal.jpeg' className='w-52 h-52 rounded' />
+                    </div>
+                    <button
+                        onClick={goToGroupExpenses}
+                        className='bg-[#1c7549] hover:bg-[#175e3a] text-white font-bold py-2 px-4 rounded  flex flex-row items-center gap-3'
+                    >
+                        <Users /> <p>Gastos Grupales</p>
+                    </button>
+                </div>
+                <div className='flex flex-col gap-3 items-center'>
+                    <div>
+                        <img src='assets/calculo.jpeg' className='w-52 h-52 rounded' />
+                    </div>
+                    <button className='bg-[#1c7549] hover:bg-[#175e3a] text-white font-bold py-2 px-4 rounded  flex flex-row items-center gap-3'>
+                        <Calculator /> <p>Calculo Financiero</p>
+                    </button>
+                </div>
             </div>
         </div>
     )
